4732a5744ede2632934bd361ba25ea4264275d91
Clay Stewart <claytonmstewart@gmail.com>
2016-10-19 00:33:44 -0500

    add routing, material design and basic components needed for app

---
+++ src/app/action-buttons/action-buttons.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ActionButtonsComponent } from './action-buttons.component';
+
+describe('Component: ActionButtons', () => {
+  it('should create an instance', () => {
+    let component = new ActionButtonsComponent();
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ActionButtonsComponent } from './action-buttons/action-buttons.component';
+
+import { MaterialModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ActionButtonsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
